6322b27ae80e0a5eabfafd6503419f3955627a1e
Nate Annau <nateannau@gmail.com>
2025-10-16 20:09:03 -0700

    Refresh examples + fix publish script bugs

--- common/scripts/publish.sh
+++ common/scripts/publish.sh
@@ -1,19 +1,21 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
+export LC_ALL=C
 SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
-VERSION=$(grep '^version = ' "$SCRIPT_DIR/typst.toml" | sed -E 's/^version = "([^"]+)"/\1/')
-TARGET_DIR="$SCRIPT_DIR/../packages/packages/preview/ergo/$VERSION"
+ROOT_DIR="$SCRIPT_DIR/../.."
+VERSION=$(grep '^version = ' "$ROOT_DIR/typst.toml" | sed -E 's/^version = "([^"]+)"/\1/')
+TARGET_DIR="$ROOT_DIR/../packages/packages/preview/ergo/$VERSION"
 
 mkdir -p $TARGET_DIR
 
-cp -r "$SCRIPT_DIR/gallery" "$SCRIPT_DIR/src" "$TARGET_DIR"
-cp "$SCRIPT_DIR/LICENSE" "$SCRIPT_DIR/.gitignore" "$SCRIPT_DIR/README.md" \
-   "$SCRIPT_DIR/lib.typ" "$SCRIPT_DIR/typst.toml" "$TARGET_DIR"
+cp -r "$ROOT_DIR/gallery" "$ROOT_DIR/src" "$TARGET_DIR"
+cp "$ROOT_DIR/LICENSE" "$ROOT_DIR/.gitignore" "$ROOT_DIR/README.md" \
+   "$ROOT_DIR/lib.typ" "$ROOT_DIR/typst.toml" "$TARGET_DIR"
 
 # MacOS and Linux sed behavior differ
 OS="$(uname -s)"
 if [[ "$OS" == "Darwin" ]]; then
-    SED_INPLACE=(-i '')
+    SED_INPLACE=(-i "")
 elif [[ "$OS" == "Linux" ]]; then
     SED_INPLACE=(-i)
 else
